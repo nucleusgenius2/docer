f6d2c370632d88e4d580a192752a743d4941c352
nucleusgenius2 <nucleusgenius@mail.ru>
2024-06-02 16:51:13 +0300

    правка конфига vite.config.js и добавления расширение php для rabbitMQ

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,38 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+    base: './',
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: '127.0.0.1',
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+            "@": fileURLToPath(new URL("resources/js/", import.meta.url)),
+        },
+    },
 });
